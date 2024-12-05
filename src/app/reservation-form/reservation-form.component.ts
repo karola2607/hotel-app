b664b8a2aa6006c1e858227e9936d2f64ab91765
Karolina Pastor <borek.karola@gmail.com>
2024-12-05 13:35:07 +0100

    Create a form group

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: false,
-  
+
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.css'
+  styleUrl: './reservation-form.component.css',
 })
 export class ReservationFormComponent {
-
+  reservationForm: FormGroup = new FormGroup({});
 }
