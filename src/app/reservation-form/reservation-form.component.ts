e6f656acbaf582abce62fd350bfeb7e04636e82c
Karolina Pastor <borek.karola@gmail.com>
2025-01-08 12:03:48 +0100

    Finish all CRUD operations with observable implementation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -30,8 +30,9 @@ export class ReservationFormComponent implements OnInit {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
 
     if (id) {
-      let reservation = this.reservationService.getReservation(id);
-      if (reservation) this.reservationForm.patchValue(reservation);
+      this.reservationService.getReservation(id).subscribe((reservation) => {
+        if (reservation) this.reservationForm.patchValue(reservation);
+      });
     }
   }
   onSubmit() {
@@ -42,10 +43,16 @@ export class ReservationFormComponent implements OnInit {
 
       if (id) {
         //Update
-        this.reservationService.updateReservation(id, reservation);
+        this.reservationService
+          .updateReservation(id, reservation)
+          .subscribe(() => {
+            console.log('Update request processed');
+          });
       } else {
         //New
-        this.reservationService.addReservation(reservation);
+        this.reservationService.addReservation(reservation).subscribe(() => {
+          console.log('Create request processed');
+        });
       }
 
       this.router.navigate(['/list']);
